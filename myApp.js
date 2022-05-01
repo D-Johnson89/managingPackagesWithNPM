538af7338663e4e0e6c5fcf2d00094cfde3ebe4b
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-05-01 19:53:43 -0400

    changed to console.log instead of bGround.log

--- myApp.js
+++ myApp.js
@@ -2,6 +2,6 @@ var express = require("express");
 var app = express();
 var bGround = require("fcc-express-bground");
 
-bGround.log("Hello World");
+console.log("Hello World");
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  * the verification process may break
  * ***************************************************/
 
-//"use strict";
+"use strict";
 
 var bGround = require("fcc-express-bground");
 var myApp = require("./myApp");
