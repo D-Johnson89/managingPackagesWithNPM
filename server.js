db4c5e0544ea72e44c355b3f2d5d849845963116
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-05-01 19:56:18 -0400

    comented out server.js to match started file more closely

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
  * the verification process may break
  * ***************************************************/
 
-"use strict";
+//"use strict";
 
 var bGround = require("fcc-express-bground");
 var myApp = require("./myApp");
-var fs = require("fs");
+//var fs = require("fs");
 var express = require("express");
 var app = express();
 
@@ -33,7 +33,7 @@ if (!process.env.DISABLE_XORIGIN) {
 	});
 }
 
-app.use("/public", express.static(process.cwd() + "/public"));
+/*app.use("/public", express.static(process.cwd() + "/public"));
 
 app.route("/_api/package.json").get(function (req, res, next) {
 	console.log("requested");
@@ -60,7 +60,7 @@ app.use(function (err, req, res, next) {
 			.type("txt")
 			.send(err.message || "SERVER ERROR");
 	}
-});
+});*/
 
 //Listen on port set in environment variable or default to 3000
 var port = process.env.PORT || 3000;
