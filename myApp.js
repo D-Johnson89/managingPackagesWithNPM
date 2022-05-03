514192629754985491771e31d94ab48080905d28
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-05-02 23:46:55 -0400

    add .

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/json", function (req, res) {
-	if (process.env.MESSAGE_STYLE === "uppercase") {
+	if (process.env.MESSAGE_STYLE == "uppercase") {
 		res.json({ message: "HELLO JSON" });
 	} else {
 		res.json({ message: "Hello json" });
