b0842b91a1c8d9b4d6f0d0d3faadcbc9ef8960ba
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-05-02 23:39:40 -0400

    add .env file and import it to myApp file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require("express");
 var app = express();
 var bGround = require("fcc-express-bground");
@@ -9,7 +10,11 @@ app.get("/", function (req, res) {
 });
 
 app.get("/json", function (req, res) {
-	res.json({ message: "Hello json" });
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({ message: "HELLO JSON" });
+	} else {
+		res.json({ message: "Hello json" });
+	}
 });
 
 app.use("/public", express.static(__dirname + "/public"));
