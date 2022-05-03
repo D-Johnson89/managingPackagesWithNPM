1c2033be0c5d60db080ea5aad18d1b615546f9b3
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-05-02 23:05:23 -0400

    add json route with dummy data

--- myApp.js
+++ myApp.js
@@ -8,6 +8,10 @@ app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", function (req, res) {
+	res.json({ message: "Hello json" });
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
