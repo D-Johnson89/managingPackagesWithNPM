cc8f37ec49727af0afb50b78a69698c0e98ac6c6
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-05-04 00:05:56 -0400

    Added chained middleware with time server

--- myApp.js
+++ myApp.js
@@ -22,6 +22,17 @@ app.get("/json", function (req, res) {
 	}
 });
 
+app.get(
+	"/now",
+	function (req, res, next) {
+		req.time = new Date().toString();
+		next();
+	},
+	function (req, res) {
+		res.json({ time: req.time });
+	}
+);
+
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
